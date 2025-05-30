a81fc545273c8a49fdcb4fc009b50d4b61397147
Force-inMotion <canzdao@gmail.com>
2025-05-30 20:14:31 +0300

    feat(project): routers products and category have been added, as well as endpoints for them

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from src.routers import category
+from src.routers import products
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI()
+app.include_router(category.router)
+app.include_router(products.router)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
